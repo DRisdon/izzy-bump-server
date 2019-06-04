f418b6a18e859ab191dbb6867933d1389c5c29d8
Daniel Risdon <drisdon07@gmail.com>
2019-06-04 15:03:27 -0400

    only allow 1 user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,59 +13,67 @@ router.get('/validate', Auth.restrict, (req, res) => {
 
 router.post('/', (req, res) => {
 
-  const email = req.body.email.toLowerCase();
-
-  const {
-    name,
-    password,
-    password_confirmation
-  } = req.body;
-
-  const errors = {
-    name: [],
-    email: [],
-    password: [],
-    password_confirmation: []
-  };
-
-  let error = false;
-  Object.keys(errors).forEach(key => {
-
-    if (!req.body[key].split(' ').join('')) {
-      errors[key].push(`${key.split('_').join(' ')} is required`);
-      error = true;
-    }
-  })
-
-  if (password !== password_confirmation) {
-    errors.password_confirmation.push("Password does not match confirmation.");
-    error = true;
-  }
-
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!re.test(email)) {
-    errors.email.push("Not a valid email address.");
-    error = true;
-  }
-
-  if (!error) {
-    console.log('name', name)
-    models.User
-      .generateToken(models.User.newUser, name, email, password)
-      .then(data => {
-        res.json({
-          id: data.id,
-          name: data.name,
-          email: data.email,
-          token: data.token
-        });
+  models.User.findAll().then(users => {
+    if (users.length > 0) {
+      res.json({
+        error: 'admin user already exists'
       })
-      .catch(err => console.log(err))
-  } else {
-    res.status(400).json({
-      errors: errors
-    })
-  }
+    } else {
+      const email = req.body.email.toLowerCase();
+
+      const {
+        name,
+        password,
+        password_confirmation
+      } = req.body;
+
+      const errors = {
+        name: [],
+        email: [],
+        password: [],
+        password_confirmation: []
+      };
+
+      let error = false;
+      Object.keys(errors).forEach(key => {
+
+        if (!req.body[key].split(' ').join('')) {
+          errors[key].push(`${key.split('_').join(' ')} is required`);
+          error = true;
+        }
+      })
+
+      if (password !== password_confirmation) {
+        errors.password_confirmation.push("Password does not match confirmation.");
+        error = true;
+      }
+
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(email)) {
+        errors.email.push("Not a valid email address.");
+        error = true;
+      }
+
+      if (!error) {
+        console.log('name', name)
+        models.User
+          .generateToken(models.User.newUser, name, email, password)
+          .then(data => {
+            res.json({
+              id: data.id,
+              name: data.name,
+              email: data.email,
+              token: data.token
+            });
+          })
+          .catch(err => console.log(err))
+      } else {
+        res.status(400).json({
+          errors: errors
+        })
+      }
+    }
+  });
 });
 
 module.exports = router;
